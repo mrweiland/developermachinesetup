c1dd9db1180f22fac55e051cb5904e11d045000d
Tobias Larsson <mrweiland@users.noreply.github.com>
2019-09-11 10:04:11 +0200

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -11,6 +11,7 @@ osascript <<EOD
     end tell
   end tell
 EOD
+sudo xcodebuild -license accept
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew install caskroom/cask/brew-cask
