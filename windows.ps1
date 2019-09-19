8e656fa75409d18b9e9d76f37b050663e01c0ab6
Tobias Larsson <mrweiland@users.noreply.github.com>
2019-09-19 21:34:44 +0200

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,9 +1,12 @@
 #Requires -RunAsAdministrator
+Set-ExecutionPolicy "RemoteSigned" -Scope Process -Confirm:$false
+Set-ExecutionPolicy "RemoteSigned" -Scope CurrentUser -Confirm:$false
+
 Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1';
 ./dotnet-install.ps1 -InstallDir '~/.dotnet' -Channel LTS;
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 RefreshEnv.cmd
 
 choco feature enable -n=allowGlobalConfirmation
@@ -15,6 +18,14 @@ choco install nodejs-lts
 choco install git
 choco install vscode
 choco install poshgit
+choco install python
+choco install anaconda3
+choco install azure-cli
+choco install azurepowershell
+choco install azure-functions-core-tools
+choco install linqpad
+choco install androidstudio
+choco install sql-server-management-studio
 choco install notepadplusplus.install
 
 choco install sysinternals
