658cae6c00d0ed6841933080fff7e7f283ff7242
Tobias Larsson <mrweiland@users.noreply.github.com>
2020-10-14 00:36:24 +0200

    Update mac.sh
    xcode-select --install
    sleep 1
    osascript <<EOD
      tell application "System Events"
        tell process "Install Command Line Developer Tools"
          keystroke return
          click button "Agree" of window "License Agreement"
        end tell
      end tell
    EOD

--- mac.sh
+++ mac.sh
@@ -1,16 +1,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 ./dotnet-install.sh --channel LTS
 
-xcode-select --install
-sleep 1
-osascript <<EOD
-  tell application "System Events"
-    tell process "Install Command Line Developer Tools"
-      keystroke return
-      click button "Agree" of window "License Agreement"
-    end tell
-  end tell
-EOD
 sudo xcodebuild -license accept
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
@@ -22,7 +12,6 @@ brew cask install google-chrome
 brew cask install powershell
 pwsh
 brew cask install android-studio
-brew cask install google-backup-and-sync
 brew cask install hyper
 brew cask install spotify
 brew cask install the-unarchiver
